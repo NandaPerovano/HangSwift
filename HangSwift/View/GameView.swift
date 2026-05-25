3ca8dc36b1786ddec693463634464da0f1db5991
NandaPerovano <nandaperovanorm@gmail.com>
2026-05-25 16:08:07 -0300

    fix: adjust NavigationBarButton

--- HangSwift/View/GameView.swift
+++ HangSwift/View/GameView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct GameView: View {
+    
+    @Environment(\.dismiss) private var dismiss
 
     @StateObject private var viewModel = HangmanViewModel()
 
@@ -62,6 +64,23 @@ struct GameView: View {
             Color(.systemGroupedBackground)
         )
         .navigationTitle("Palavra Secreta")
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+
+            ToolbarItem(placement: .topBarLeading) {
+
+                Button {
+
+                    dismiss()
+
+                } label: {
+
+                    Image(systemName: "chevron.left")
+                        .font(.headline)
+                        .foregroundStyle(.indigo)
+                }
+            }
+        }
         .navigationBarTitleDisplayMode(.inline)
         .alert(
             viewModel.resultTitle,
